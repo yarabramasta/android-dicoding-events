31e46a55c356f8c91b5c2af06ba458a0853b5b4c
yarabramasta <yarabram111@gmail.com>
2024-10-11 18:14:22 +0700

    fix: missing google font dependency

--- app/src/main/java/dev/ybrmst/dicoding_events/ui/theme/Type.kt
+++ app/src/main/java/dev/ybrmst/dicoding_events/ui/theme/Type.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
+import dev.ybrmst.dicoding_events.R
 
 val provider = GoogleFont.Provider(
   providerAuthority = "com.google.android.gms.fonts",
