f1633fdcda0c9bc01a1fc3803d190349d6bc1942
yarabramasta <yarabram111@gmail.com>
2024-10-14 17:03:03 +0700

    fix(EventErrorFallback.kt): remove unused import

--- app/src/main/java/dev/ybrmst/dicoding_events/ui/composables/event/EventErrorFallback.kt
+++ app/src/main/java/dev/ybrmst/dicoding_events/ui/composables/event/EventErrorFallback.kt
@@ -1,6 +1,5 @@
 package dev.ybrmst.dicoding_events.ui.composables.event
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
