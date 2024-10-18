382ff3f2064a8bad43ae679a7699dcc564c046ef
yarabramasta <yarabram111@gmail.com>
2024-10-18 11:23:50 +0700

    chore: bump ksp & kotlin version to v2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
   alias(libs.plugins.jetbrains.kotlin.android)
   alias(libs.plugins.kotlin.serialization)
   alias(libs.plugins.ksp)
+  alias(libs.plugins.compose.compiler)
 }
 
 android {
